6e1e243be371b6e157d8633ab77cbda895c55af9
Руслан Усманов <extiser@MacBook-Pro-Ruslan.local>
2015-08-24 07:50:19 +0500

    fixes & new pages

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -111,4 +111,13 @@ jQuery(document).ready(function ($) {
         $(this).toggleClass('active');
         $('.top-menu__list').toggleClass('active');
     });
+
+    $('.top-menu__item > a').click(function () {
+        $(this).toggleClass('active').parent().find('.top-menu-inside').toggleClass('active');
+        $('.mask').addClass('active');
+    });
+
+    $('.mask').click(function () {
+        $('.mask, .top-menu-inside, .top-menu__item > a').removeClass('active');
+    });
 });
